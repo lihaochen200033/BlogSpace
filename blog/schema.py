8a0120d5749b285729ad5bf996312bef389a71ea
lihaochen200033 <lihaochen200033@github.com>
2023-05-18 21:51:27 -0400

    added api for view all posts and individual post by id

--- blog/schema.py
+++ blog/schema.py
@@ -1,5 +1,26 @@
 import graphene
 from graphene_django import DjangoObjectType
+from .models import Post, Comment
+
+class PostType(DjangoObjectType):
+    class Meta:
+        model = Post
+        fields = ("title", "content", "author", "created_on", "updated_on")
+
+class CommentType(DjangoObjectType):
+    class Meta:
+        model = Comment
+        fields = ("post", "author", "content", "created_on", "updated_on")
 
 class Query(graphene.ObjectType):
-    pass
+    all_posts = graphene.List(PostType)
+    post_by_id = graphene.Field(PostType, post_id=graphene.ID(required=True))
+
+    def resolve_all_posts(root, info):
+        return Post.objects.all()
+
+    def resolve_post_by_id(root, info, post_id):
+        try:
+            return Post.objects.get(id=post_id)
+        except Post.DoesNotExist:
+            return None
